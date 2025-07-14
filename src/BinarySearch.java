a3c346ef42a4ad2cb9a4ed0914811490a036d4de
anuoluwa01 <anu.ogunnubi@gmail.com>
2025-07-14 21:55:44 +0000

    Updated binary search, shell sort, and main class

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -16,7 +16,7 @@ public class BinarySearch {
         } return -1;
     }
 
-    public static void main(String[] args) {
+    public void execute() {
         Scanner sc = new Scanner(System.in);
         int n = 0;
 
